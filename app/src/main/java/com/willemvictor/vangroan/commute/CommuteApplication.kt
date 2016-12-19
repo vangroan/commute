f8a0c6b75ef6862fe5208524a9010ed7f97fd19c
Willem Victor <wimpievictor@gmail.com>
2016-12-19 16:26:16 +0200

    Project create

---
+++ app/src/main/java/com/willemvictor/vangroan/commute/CommuteApplication.kt
@@ -0,0 +1,11 @@
+package com.willemvictor.vangroan.commute
+
+import android.app.Application
+
+/**
+ * Created by Victor on 2016/12/19.
+ */
+
+class CommuteApplication : Application() {
+
+}
---
+++ app/src/main/java/com/willemvictor/vangroan/commute/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.willemvictor.vangroan.commute.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.willemvictor.vangroan.commute.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
